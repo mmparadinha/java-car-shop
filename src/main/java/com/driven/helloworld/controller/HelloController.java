8139b0518be51ddc713864fb5bbec160b408ab25
mmparadinha <matheusmparadinha@gmail.com>
2023-03-28 18:27:25 -0300

    first java spring project

---
+++ src/main/java/com/driven/helloworld/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.driven.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+  @GetMapping
+  public String HelloSpring() {
+    return "Olá, Spring!";
+  }
+}
